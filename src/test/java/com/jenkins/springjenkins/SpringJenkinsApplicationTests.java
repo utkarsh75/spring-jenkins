1494cf5a5776f2fb7b0c127c3da44530a7d1761a
utkarsh75 <utkarshbhujbal75@gmail.com>
2022-11-08 14:14:30 +0530

    first-commit

---
+++ src/test/java/com/jenkins/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jenkins.springjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class SpringJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
